e9e7d891f10d804f8c1786ca805fdc7a5b3cf4fc
sam_bland <sbland.co.uk@gmail.com>
2020-12-22 21:56:51 +0000

    Add py_interface mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,18 @@
 extern crate pyo3;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-use pyo3::PyObjectProtocol;
 
 mod process_runner;
-use geo::point;
+mod py_interface;
+
 use process_runner::example_process;
 use process_runner::population_migration;
 use process_runner::run_iteration;
-use process_runner::Process;
-
-use process_runner::state::CellState;
 use process_runner::state::GlobalData;
 use process_runner::state::IterationState;
+use process_runner::Process;
 
-#[pyclass]
-#[derive(Clone)]
-pub struct CellStatePy {
-    // #[pyo3(get)]
-    pub inner: CellState,
-}
-
-#[pymethods]
-impl CellStatePy {
-    #[new]
-    fn new(id: u32, pos: (f64, f64), population: u32) -> Self {
-        CellStatePy {
-            inner: CellState {
-                id,
-                position: point!(x: pos.0, y: pos.1),
-                population,
-            },
-        }
-    }
-}
-
-#[pyproto]
-impl PyObjectProtocol for CellStatePy {
-    fn __str__(&self) -> PyResult<&'static str> {
-        Ok("CellStatePy")
-    }
-
-    fn __repr__<'a>(&'a self) -> PyResult<String> {
-        Ok(format!("CellStateObj id: {}", self.inner.id))
-    }
-
-    fn __getattr__(&'a self, name: &str) -> PyResult<String> {
-        let out: String = match name {
-            "a" => format!("hello"),
-            "population" => format!("{}", self.inner.population),
-            &_ => format!("World"),
-        };
-        Ok(out)
-    }
-}
-
-#[pyclass]
-#[derive(Clone)]
-pub struct GlobalDataPy {
-    pub inner: GlobalData,
-}
+use py_interface::CellStatePy;
 
 #[pyfunction]
 fn run_iteration_py(
@@ -70,7 +23,7 @@ fn run_iteration_py(
     // ) -> PyResult<Vec<CellStatePy>> {
 ) -> PyResult<Vec<CellStatePy>> {
     // ) -> PyResult<(Vec<CellState>, GlobalData, Vec<Vec<u32>>)> {
-    let cell_data_inner: Vec<CellState> = cell_data.iter().map(|c| c.inner).collect::<Vec<_>>();
+    let cell_data_inner = cell_data.iter().map(|c| c.inner).collect::<Vec<_>>();
     let initial_state = IterationState {
         global_data: GlobalData { iterations: 0 },
         cells: cell_data_inner,
