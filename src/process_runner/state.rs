00f00703a8a8b518709075d54131b9eba0248162
sam_bland <sbland.co.uk@gmail.com>
2021-01-14 20:01:22 +0000

    implement action and cellstate in macro

--- src/process_runner/state.rs
+++ src/process_runner/state.rs
@@ -11,78 +11,98 @@ pub struct GlobalState {
 #[derive(Debug, Clone, PartialEq, Copy)]
 pub struct CellIndex(pub u32);
 
-#[derive(Debug, Clone, PartialEq, Copy)]
-pub struct CellState {
-    pub id: CellIndex,
-    pub position: Point<f64>,
-    pub population: u32,
-    pub population_attraction: f64,
-    pub residential_capacity: u32,
-    pub population_birth_rate: f64,
-    pub population_death_rate: f64,
+macro_rules! create_struct {
+    ($cls: ident, [$(($ismut:ident $field_name:ident, $type:ty, $default:expr))*]) => {
+        #[derive(Debug, Clone, PartialEq, Copy)]
+        pub struct CellState {
+            $(pub $field_name: $type),*
+        }
+    };
 }
 
-impl Default for CellState {
-    fn default() -> CellState {
-        CellState {
-            id: CellIndex(0),
-            position: point!(x:0.0, y:0.0),
-            population: 0,
-            population_attraction: 1.0,
-            residential_capacity: 0,
-            population_birth_rate: 1.0,
-            population_death_rate: 0.2,
+macro_rules! impl_default {
+    ($cls: ident, [$(($ismut:ident $field_name:ident, $type:ty, $default:expr))*]) => {
+        impl Default for $cls {
+            fn default() -> $cls {
+                $cls {
+                    $($field_name: $default),*
+                }
+            }
         }
-    }
+    };
 }
 
-impl CellState {
-    /// Apply CellUpdates to the cell
-    pub fn apply(&mut self, cell_action: &CellUpdate) {
-        macro_rules! action {
-            (Action::ADD, $field_name:ident, $type:ty) => {
-                self.$field_name = (self.$field_name as i32 + i32::from(cell_action.value)) as $type
-            };
-            (Action::SET, $field_name:ident, $type:ty) => {
-                self.$field_name = <$type>::from(cell_action.value)
-            };
-        }
-
-        macro_rules! actions {
-            (Action::ADD, [$($field_name:ident, $type:ty), *]) => {
-                match cell_action.target_field.as_str() {
-                    $(stringify!($field_name) => action!(Action::ADD, $field_name, $type)),*,
-                    &_ => (),
-                }
-            };
-            (Action::SET, [$($field_name:ident, $type:ty), *]) => {
-                match cell_action.target_field.as_str() {
-                    $(stringify!($field_name) => action!(Action::SET, $field_name, $type)),*,
-                    &_ => (),
-                }
-            };
-        }
-
-        macro_rules! actions_full {
-            ($x: path, [$(($field_name:ident, $type:ty)), *]) => {
-                match $x {
-                    Action::ADD => actions!(Action::ADD, [$($field_name, $type),*]),
-                    Action::SET => actions!(Action::SET, [$($field_name, $type),*]),
-                }
-            };
-        }
-        let action = &cell_action.action;
-        // TODO: implement for all cell fields
-        actions_full!(
-            action,
-            [
-                (population, u32),
-                (population_attraction, f64),
-                (residential_capacity, u32)
-            ]
-        );
-    }
+macro_rules! action {
+    (ismut Action::ADD, $field_name:ident, $type:ty, $Self: ident, $cell_action: ident) => {
+        $Self.$field_name = ($Self.$field_name as i32 + i32::from($cell_action.value)) as $type
+    };
+    (ismut Action::SET, $field_name:ident, $type:ty, $Self: ident, $cell_action: ident) => {
+        $Self.$field_name = <$type>::from($cell_action.value)
+    };
+    // (cons $_:stmt) => {
+    //     panic!()
+    // };
+    (cons $_:path, $__:ident, $___:ty, $____: ident, $_____: ident) => {
+        panic!()
+    };
 }
+
+macro_rules! actions {
+    // ($action_type:tt, [$(($mut: ident $field_name:ident, $type:ty, $default:expr)),*], $Self: ident, $cell_action: ident) => {
+    //     match $cell_action.target_field.as_str() {
+    //         $(stringify!($field_name) => action!($mut $action_type, $field_name, $type, $Self, $cell_action)),*,
+    //         &_ => (),
+    //     }
+    // };
+    (Action::ADD, [$(($mut: ident $field_name:ident, $type:ty, $default:expr)),*], $Self: ident, $cell_action: ident) => {
+        match $cell_action.target_field.as_str() {
+            $(stringify!($field_name) => action!($mut Action::ADD, $field_name, $type, $Self, $cell_action)),*,
+            &_ => (),
+        }
+    };
+    (Action::SET, [$(($mut: ident $field_name:ident, $type:ty, $default:expr)),*], $Self: ident, $cell_action: ident) => {
+        match $cell_action.target_field.as_str() {
+            $(stringify!($field_name) => action!($mut Action::SET, $field_name, $type, $Self, $cell_action)),*,
+            &_ => (),
+        }
+    };
+}
+
+macro_rules! impl_cell_state_apply {
+    ($cls: ident, [$($field_info:tt) *]) => {
+        impl $cls {
+            /// Apply CellUpdates to the cell
+            pub fn apply(&mut self, cell_action: &CellUpdate) {
+                let action = &cell_action.action;
+                match action {
+                    Action::ADD => actions!(Action::ADD, [$($field_info), *], self, cell_action),
+                    Action::SET => actions!(Action::SET, [$($field_info), *], self, cell_action),
+                }
+            }
+        }
+    };
+}
+
+macro_rules! impl_struct {
+    ($cls: ident, [$($field_info:tt),*]) => {
+        create_struct!($cls, [$($field_info)*]);
+        impl_default!($cls, [$($field_info)*]);
+        impl_cell_state_apply!($cls, [$($field_info)*]);
+    };
+}
+
+impl_struct!(
+    CellState,
+     [
+        (cons id, CellIndex, CellIndex(0)),
+        (cons position, Point<f64>, point!(x:0.0, y:0.0)),
+        (ismut population, u32, 0),
+        (ismut population_attraction, f64, 1.0),
+        (ismut residential_capacity, u32, 0),
+        (ismut population_birth_rate, f64, 1.0),
+        (ismut population_death_rate, f64, 0.0)
+    ]
+);
 
 #[allow(dead_code)]
 impl CellState {
