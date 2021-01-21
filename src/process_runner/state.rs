7d0c879177a26ca864d362ee38637b4230b62f0b
sam_bland <sbland.co.uk@gmail.com>
2021-01-21 11:12:39 +0000

    Refactor cell state macros

--- src/process_runner/state.rs
+++ src/process_runner/state.rs
@@ -11,6 +11,15 @@ pub struct GlobalState {
 #[derive(Debug, Clone, PartialEq, Copy)]
 pub struct CellIndex(pub u32);
 
+///
+///  #[derive(Debug, Clone, PartialEq, Copy)]
+///  pub struct CellState {
+///      pub id: CellIndex,
+///      pub position: Point<f64>,
+///      pub population: u32,
+///      ...
+///  }
+///
 macro_rules! create_struct {
     ($cls: ident, [$(($ismut:ident $field_name:ident, $type:ty, $default:expr))*]) => {
         #[derive(Debug, Clone, PartialEq, Copy)]
@@ -20,6 +29,17 @@ macro_rules! create_struct {
     };
 }
 
+/*
+    impl Default for CellState {
+        fn default() -> CellState {
+            CellState {
+                id: CellIndex(0),
+                position: point!(x:0.0, y:0.0),
+                ...
+            }
+        }
+    }
+*/
 macro_rules! impl_default {
     ($cls: ident, [$(($ismut:ident $field_name:ident, $type:ty, $default:expr))*]) => {
         impl Default for $cls {
@@ -32,7 +52,12 @@ macro_rules! impl_default {
     };
 }
 
+/// Apply the action to the field
+///
+/// This performs some type conversions and modifies the field
+/// The target field must be mutable
 macro_rules! action {
+    // If Action::ADD then we simply add
     (ismut Action::ADD, $field_name:ident, $type:ty, $Self: ident, $cell_action: ident) => {
         $Self.$field_name = ($Self.$field_name as i32 + i32::from($cell_action.value)) as $type
     };
@@ -47,27 +72,28 @@ macro_rules! action {
     };
 }
 
-macro_rules! actions {
-    // ($action_type:tt, [$(($mut: ident $field_name:ident, $type:ty, $default:expr)),*], $Self: ident, $cell_action: ident) => {
-    //     match $cell_action.target_field.as_str() {
-    //         $(stringify!($field_name) => action!($mut $action_type, $field_name, $type, $Self, $cell_action)),*,
-    //         &_ => (),
-    //     }
-    // };
-    (Action::ADD, [$(($mut: ident $field_name:ident, $type:ty, $default:expr)),*], $Self: ident, $cell_action: ident) => {
+/// Match a field to an action
+///
+/// Example Output:
+/// ```
+/// match cell_action.target_field.as_str() {
+///     "population" => {
+///         self.population = (self.population as i32 + i32::from(cell_action.value)) as u32
+///     }
+///     ...
+///     &_ => (),
+/// }
+/// ```
+macro_rules! map_action_to_fields {
+    ((Action::$action_type: ident), [$(($mut: ident $field_name:ident, $type:ty, $default:expr)),*], $Self: ident, $cell_action: ident) => {
         match $cell_action.target_field.as_str() {
-            $(stringify!($field_name) => action!($mut Action::ADD, $field_name, $type, $Self, $cell_action)),*,
-            &_ => (),
-        }
-    };
-    (Action::SET, [$(($mut: ident $field_name:ident, $type:ty, $default:expr)),*], $Self: ident, $cell_action: ident) => {
-        match $cell_action.target_field.as_str() {
-            $(stringify!($field_name) => action!($mut Action::SET, $field_name, $type, $Self, $cell_action)),*,
+            $(stringify!($field_name) => action!($mut Action::$action_type, $field_name, $type, $Self, $cell_action)),*,
             &_ => (),
         }
     };
 }
 
+/// Implement the apply action method
 macro_rules! impl_cell_state_apply {
     ($cls: ident, [$($field_info:tt) *]) => {
         impl $cls {
@@ -75,10 +101,70 @@ macro_rules! impl_cell_state_apply {
             pub fn apply(&mut self, cell_action: &CellUpdate) {
                 let action = &cell_action.action;
                 match action {
-                    Action::ADD => actions!(Action::ADD, [$($field_info), *], self, cell_action),
-                    Action::SET => actions!(Action::SET, [$($field_info), *], self, cell_action),
+                    // Map each field to each action
+                    Action::ADD => map_action_to_fields!((Action::ADD), [$($field_info), *], self, cell_action),
+                    Action::SET => map_action_to_fields!((Action::SET), [$($field_info), *], self, cell_action),
                 }
             }
+        }
+    };
+}
+
+/// Implement the new associate function
+macro_rules! impl_new {
+    ($cls: ident, [$(($mut: ident $field_name:ident, $type:ty, $default:expr)),*]) => {
+        pub fn new(
+            // TODO: We currently need to redefine the fields here
+            // $($field_name:$type),
+            id: u32,
+            pos: impl Into<Option<Point<f64>>>,
+            population: impl Into<Option<u32>>,
+            population_attraction: impl Into<Option<f64>>,
+            residential_capacity: impl Into<Option<u32>>,
+            population_birth_rate: impl Into<Option<f64>>,
+            population_death_rate: impl Into<Option<f64>>,
+        ) -> CellState {
+            CellState {
+                // $($field_name:$default),
+                id: CellIndex(id),
+                position: pos.into().unwrap_or(point!(x: 0.0, y: 0.0)),
+                population: population.into().unwrap_or(0),
+                population_attraction: population_attraction.into().unwrap_or(1.0),
+                residential_capacity: residential_capacity.into().unwrap_or(0),
+                population_birth_rate: population_birth_rate.into().unwrap_or(1.0),
+                population_death_rate: population_death_rate.into().unwrap_or(0.2),
+            }
+        }
+    };
+}
+
+/// Implement class functions
+macro_rules! impl_cls_funcs {
+    ($cls: ident, [$($field_info:tt) *]) => {
+        #[allow(dead_code)]
+        impl $cls {
+            impl_new!($cls, [$($field_info), *]);
+
+            pub fn new_default(id: u32) -> $cls {
+                $cls {
+                    id: CellIndex(id),
+                    ..Default::default()
+                }
+            }
+
+            // TODO: Implement random generation
+            // pub fn new_random(id: u32) -> $cls {
+            //     $cls {
+            //         $($field_name: $default),*
+            //         id: CellIndex(id),
+            //         position: point!(x: 0.0, y: 0.0),
+            //         population: 100,
+            //         population_attraction: 1.0,
+            //         residential_capacity: 100,
+            //         population_birth_rate: 1.0,
+            //         population_death_rate: 0.2,
+            //     }
+            // }
         }
     };
 }
@@ -88,6 +174,7 @@ macro_rules! impl_struct {
         create_struct!($cls, [$($field_info)*]);
         impl_default!($cls, [$($field_info)*]);
         impl_cell_state_apply!($cls, [$($field_info)*]);
+        impl_cls_funcs!($cls, [$($field_info)*]);
     };
 }
 
@@ -103,50 +190,6 @@ impl_struct!(
         (ismut population_death_rate, f64, 0.0)
     ]
 );
-
-#[allow(dead_code)]
-impl CellState {
-    pub fn new(
-        id: u32,
-        pos: Point<f64>,
-        population: impl Into<Option<u32>>,
-        population_attraction: impl Into<Option<f64>>,
-        residential_capacity: impl Into<Option<u32>>,
-        population_birth_rate: impl Into<Option<f64>>,
-        population_death_rate: impl Into<Option<f64>>,
-    ) -> CellState {
-        CellState {
-            id: CellIndex(id),
-            position: pos,
-            population: population.into().unwrap_or(0),
-            population_attraction: population_attraction.into().unwrap_or(1.0),
-            residential_capacity: residential_capacity.into().unwrap_or(0),
-            population_birth_rate: population_birth_rate.into().unwrap_or(1.0),
-            population_death_rate: population_death_rate.into().unwrap_or(0.2),
-        }
-    }
-
-    pub fn new_default(id: u32) -> CellState {
-        CellState {
-            // TODO: Implement random generation
-            id: CellIndex(id),
-            ..Default::default()
-        }
-    }
-
-    pub fn new_random(id: u32) -> CellState {
-        CellState {
-            // TODO: Implement random generation
-            id: CellIndex(id),
-            position: point!(x: 0.0, y: 0.0),
-            population: 100,
-            population_attraction: 1.0,
-            residential_capacity: 100,
-            population_birth_rate: 1.0,
-            population_death_rate: 0.2,
-        }
-    }
-}
 
 #[derive(Clone)]
 pub struct IterationState {
