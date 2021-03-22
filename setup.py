6c97d5ecb988ba9cce472a27dcd862e82b006baf
Sam Bland <sbland.co.uk@gmail.com>
2021-03-22 14:03:15 +0000

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cellular_automata_pyinterface",
-    version="0.3.0",
+    version="0.3.1",
     author="sam bland",
     author_email="sam.bland@sei.org",
     description="Cellular Automata Agent model",
